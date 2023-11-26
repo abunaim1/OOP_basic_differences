5219b9502bec19dd3df88326db56b03c8158298b
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2023-11-26 18:18:36 +0600

    done

--- overload_Vs_override.py
+++ overload_Vs_override.py
@@ -35,3 +35,28 @@ class Swimmer(Person):
 
 s1 = Swimmer('Atik', 22, 'chipa')
 print(s1.exersize()) # this exersize method overrride from person class. 
+
+
+#Find out which of these players is older using the operator overloading.
+
+class Person:
+    def __init__(self, name, age, height, weight) -> None:
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
+
+class Cricketer(Person):
+    def __init__(self, name, age, height, weight) -> None:
+        super().__init__(name, age, height, weight)
+    
+    def __gt__(self, other):
+        return self.age > other.age
+
+shakib = Cricketer('Sakib', 38, 68, 91)
+musfiq = Cricketer('musfiq', 36, 68, 88)
+kamal = Cricketer('Kamal', 39, 68, 94)
+jack = Cricketer('Jack', 38, 68, 91)
+kalam = Cricketer('Kalam', 37, 68, 95)
+
+print(kamal > jack)
